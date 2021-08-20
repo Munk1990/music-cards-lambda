28736b9b31e4b1fc8c32dcddc09cf9abc466403e
Mayank Kumar <mayank.kr@pm.me>
2021-08-20 19:08:52 +0530

    Card level caching

--- music_cards_service/api/webapi.py
+++ music_cards_service/api/webapi.py
@@ -1,13 +1,23 @@
+import io
 import math
+import os
 import time
 import traceback
 
+import boto3
 from PIL import Image
 
 from imager.cardcreator import generate_card_front, generate_card_back, generate_message_card
 from metadata.albumdetails import search_songlink_album
 
 SUPPORTED_MUSIC_SERVICES = {"apple": search_songlink_album}
+CARD_GENERATION = {"back": generate_card_back, "front": generate_card_front}
+
+S3_CACHE_BUCKET = os.environ['CACHE_BUCKET']
+S3_CACHE_LOCATION = "cards/"
+
+TMP_FILE_PATH = "/tmp/"
+IMAGE_EXTN = ".jpg"
 
 page_dimension = [203, 277]
 dpi = 300
@@ -29,28 +39,20 @@ print(
 def get_front_page(cards_per_page, music_service, entities, output_path):
     if cards_per_page > 8:
         raise UnboundLocalError("Can only process up to 8 cards")
-    get_album_details = SUPPORTED_MUSIC_SERVICES[music_service.lower()]
     front_page = Image.new('RGB', (widthpixel, heightpixel), (255, 255, 255))
-    error_count = 0
     for counter in range(len(entities)):
         try:
-            album_details = get_album_details(entities[counter])
-            album_front_image = generate_card_front(album_details, cardwidth, cardheight)
+            card = get_cache_or_generate_card(music_service, entities[counter], cardwidth, cardheight, "front")
         except Exception as e:
             error_message = "Unable to process entity [%s]\n" % entities[
                 counter] + "Error Stack trace:\n%s" % traceback.format_exc()
             print(error_message)
-            album_front_image = generate_message_card(error_message, cardwidth, cardheight)
-            error_count = error_count + 1
-        album_front_image = album_front_image.rotate(90, expand=True)
+            card = generate_message_card(error_message, cardwidth, cardheight)
+        card = card.rotate(90, expand=True)
         x = (counter % 2) * cardheight
         y = math.floor(counter / 2) * cardwidth
-        front_page.paste(album_front_image, (x + print_margin_left, y))
+        front_page.paste(card, (x + print_margin_left, y))
     front_page.save(output_path)
-    if error_count > 0:
-        return "_" + str(error_count) + "err"
-    else:
-        return ""
 
 
 def get_back_page(cards_per_page, music_service, entities, output_path):
@@ -58,25 +60,53 @@ def get_back_page(cards_per_page, music_service, entities, output_path):
         raise UnboundLocalError("Can only process 8 card template")
     if len(entities) > 8:
         raise UnboundLocalError("Can only process upto 8 cards")
-    get_album_details = SUPPORTED_MUSIC_SERVICES[music_service.lower()]
     back_page = Image.new('RGB', (widthpixel, heightpixel), (255, 255, 255))
-    error_count = 0
-
     for counter in range(len(entities)):
         try:
-            album_details = get_album_details(entities[counter])
-            album_back_image = generate_card_back(album_details, cardwidth, cardheight)
+            card = get_cache_or_generate_card(music_service, entities[counter], cardwidth, cardheight, "back")
         except Exception as e:
             error_message = "Unable to process entity [%s]\n" % entities[
                 counter] + "Error Stack trace:\n%s" % traceback.format_exc()
             print(error_message)
-            album_back_image = generate_message_card(error_message, cardwidth, cardheight)
-        album_back_image = album_back_image.rotate(270, expand=True)
+            card = generate_message_card(error_message, cardwidth, cardheight)
+        card = card.rotate(270, expand=True)
         x = ((counter + 1) % 2) * cardheight
         y = math.floor(counter / 2) * cardwidth
-        back_page.paste(album_back_image, (x, y))
+        back_page.paste(card, (x, y))
     back_page.save(output_path)
-    if error_count > 0:
-        return "_" + str(error_count) + "err"
-    else:
-        return ""
+
+
+def get_cache_or_generate_card(music_service, entity, width, height, variant):
+    file_id = "%s_%s_%s_%s_%s.jpg" % (music_service, entity, width, height, variant)
+    s3_location = S3_CACHE_LOCATION + file_id
+    try:
+        img = fetch_from_cache(S3_CACHE_BUCKET, s3_location)
+        print("Cache hit [%s]" % s3_location)
+        return img
+    except:
+        print("Cache miss [%s]" % s3_location)
+        get_album_details = SUPPORTED_MUSIC_SERVICES[music_service.lower()]
+        album_details = get_album_details(entity)
+        img = CARD_GENERATION[variant](album_details, width, height)
+        img.save(TMP_FILE_PATH + variant + IMAGE_EXTN)
+        copy_to_s3(TMP_FILE_PATH + variant + IMAGE_EXTN, S3_CACHE_BUCKET, s3_location)
+        return img
+
+
+def fetch_from_cache(bucket, key):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket)
+    obj = bucket.Object(key)
+    response = obj.get()
+    file_stream = response['Body']
+    im = Image.open(file_stream)
+    return im
+
+
+def copy_to_s3(temp_file_location, s3_bucket, s3_path):
+    s3 = boto3.resource('s3')
+    print("Copying file from [%s] to bucket [%s] key [%s]" % (temp_file_location, s3_bucket, s3_path))
+    s3.meta.client.upload_file(temp_file_location, s3_bucket, s3_path)
+
+
+#get_front_page(8, "apple", ['880709749', '1103770960', '1569562341'], "testout2.jpg")
--- music_cards_service/app.py
+++ music_cards_service/app.py
@@ -8,7 +8,7 @@ import boto3
 # import requests
 import botocore
 
-from api.webapi import get_front_page, get_back_page
+from api.webapi import get_front_page, get_back_page, copy_to_s3
 
 S3_CACHE_BUCKET = os.environ['CACHE_BUCKET']
 IMAGE_DIRECTORY = "files/"
@@ -21,20 +21,15 @@ IMAGE_EXTENSION = ".jpg"
 
 
 def get_front(event, context):
-    print(event)
     albumlist = event["queryStringParameters"]["albums"].split(",")
     print("Printing front page for albums: %s" % albumlist)
 
     file_id = IMAGE_DIRECTORY + FRONT_TAG + "_" + ("_".join("{0}".format(n) for n in albumlist))
     s3_path = file_id + IMAGE_EXTENSION
-    if cache_exists(s3_path):
-        print("File %s exists" % (file_id + IMAGE_EXTENSION))
-    else:
-        print("File %s does not exist" % (file_id + IMAGE_EXTENSION))
-        append_message = get_front_page(8, "apple", albumlist, TMP_FILE_LOCATION)
-        s3_path = file_id + append_message + IMAGE_EXTENSION
-        print("Copying to %s" % s3_path)
-        copy_to_s3(TMP_FILE_LOCATION, S3_CACHE_BUCKET, s3_path)
+
+    get_front_page(8, "apple", albumlist, TMP_FILE_LOCATION)
+    print("Copying to %s" % s3_path)
+    copy_to_s3(TMP_FILE_LOCATION, S3_CACHE_BUCKET, s3_path)
     s3_url = S3_LINK_TEMPLATE + s3_path
     return {
         "statusCode": 302,
@@ -46,20 +41,15 @@ def get_front(event, context):
 
 
 def get_back(event, context):
-    print(event)
     albumlist = event["queryStringParameters"]["albums"].split(",")
     print("Printing back page for albums: %s" % albumlist)
 
     file_id = IMAGE_DIRECTORY + BACK_TAG + "_" + ("_".join("{0}".format(n) for n in albumlist))
     s3_path = file_id + IMAGE_EXTENSION
-    if cache_exists(file_id):
-        print("File %s exists" % (file_id + IMAGE_EXTENSION))
-    else:
-        print("File %s does not exist" % (file_id + IMAGE_EXTENSION))
-        append_message = get_back_page(8, "apple", albumlist, TMP_FILE_LOCATION)
-        s3_path = file_id + append_message + IMAGE_EXTENSION
-        print("Copying to %s" % s3_path)
-        copy_to_s3(TMP_FILE_LOCATION, S3_CACHE_BUCKET, s3_path)
+
+    get_back_page(8, "apple", albumlist, TMP_FILE_LOCATION)
+    print("Copying to %s" % s3_path)
+    copy_to_s3(TMP_FILE_LOCATION, S3_CACHE_BUCKET, s3_path)
     s3_url = S3_LINK_TEMPLATE + s3_path
     return {
         "statusCode": 302,
@@ -70,20 +60,3 @@ def get_back(event, context):
     }
 
 
-def cache_exists(path):
-    s3 = boto3.resource('s3')
-    try:
-        s3.Object(S3_CACHE_BUCKET, path).load()
-        return True
-    except:
-        return False
-
-
-def copy_to_s3(temp_file_location, s3_bucket, s3_path):
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(temp_file_location, s3_bucket, s3_path)
-
-
-def retrieve_image(s3_bucket, s3_path):
-    s3 = boto3.resource('s3')
-    s3.meta.client
